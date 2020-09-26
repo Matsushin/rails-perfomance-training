9b2333c4d860f9da74e6326d767afdf8e3dec5c5
Matsushin <matsu1983@gmail.com>
2020-09-26 19:46:37 +0900

    課題2：担当者名の表示で発生するN+1を解消しよう

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.order(id: :desc).page(params[:page]).per(200)
+    @tasks = Task.includes(:category, :users).order(id: :desc).page(params[:page]).per(200)
   end
 end
